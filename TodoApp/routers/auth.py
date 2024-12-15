e553d2b6a011780cf9d474787fb0a2069e3a9355
Suman9038 <sumanjhad903878@gmail.com>
2024-12-15 13:57:39 +0530

    Login Path operation addded

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,APIRouter,HTTPException,Response,status,Depends
 from sqlalchemy.orm import Session
 from ..import schema,database,models,utils
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 
 
@@ -18,3 +19,13 @@ def user_registration(user : schema.CreateUser,db :Session=Depends(database.get_
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.post("/login")
+def user_login(user_credentials: OAuth2PasswordRequestForm =Depends(),db : Session = Depends(database.get_db)) :
+    user = db.query(models.User).filter(models.User.username==user_credentials.username).first()
+    if not user :
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"User Not Found With this Username")
+    if not utils.verify(user_credentials.password , user.password) :
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"User Not Found With this Password ")
+        
+    return{"access_token" : "Example Token"}
--- TodoApp/utils.py
+++ TodoApp/utils.py
@@ -4,3 +4,6 @@ pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
 
 def hash(password : str) : 
     return pwd_context.hash(password)
+
+def verify(user_password : str, hashed_password) : 
+    return pwd_context.verify(user_password,hashed_password)
